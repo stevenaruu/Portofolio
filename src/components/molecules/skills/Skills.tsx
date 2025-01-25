5a5a7d135d76e105aa0edaa11d898a5891c68d30
Steven <steven@binus.edu>
2025-01-26 02:55:12 +0700

    style: height full while < tab device

--- src/components/molecules/skills/Skills.tsx
+++ src/components/molecules/skills/Skills.tsx
@@ -6,7 +6,7 @@ const Skills = () => {
   return (
     <Container>
       <Title text="Skills." />
-      <div className='mt-5 justify-center flex h-44 gap-2 flex-wrap lg:flex-nowrap'>
+      <div className='mt-5 justify-center flex lg:h-44 gap-2 flex-wrap lg:flex-nowrap'>
         <img className='w-full lg:w-auto' src="https://github-readme-stats.vercel.app/api/top-langs/?username=stevenaruu&layout=compact&langs_count=20&theme=dark" />
         <img className='w-full lg:w-auto' src="https://github-readme-stats.vercel.app/api?username=stevenaruu&rank_icon=github&theme=dark" />
         <GitHubCalendar blockSize={12} colorScheme='dark' username='stevenaruu' />
