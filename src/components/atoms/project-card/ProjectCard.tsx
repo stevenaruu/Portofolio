82ff7574cc5dc817477eb005ef86af86d76d7faa
Steven <steven@binus.edu>
2025-01-27 14:37:58 +0700

    style: unused variable

--- src/components/atoms/project-card/ProjectCard.tsx
+++ src/components/atoms/project-card/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import { BorderBeam } from '../border-beam/BorderBeam'
 import { openInNewTab } from '../../../lib/utils'
 
