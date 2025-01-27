aea878098224297ae4f8dd7a38a1e949c0167d6a
Steven <steven@binus.edu>
2025-01-27 21:43:06 +0700

    style: project section responsive on < tab

--- src/components/atoms/project-card/ProjectCard.tsx
+++ src/components/atoms/project-card/ProjectCard.tsx
@@ -2,23 +2,26 @@ import { ReactNode } from 'react'
 import { BorderBeam } from '../border-beam/BorderBeam'
 import { openInNewTab } from '../../../lib/utils'
 
-const ProjectCard = ({ children, title, description, image, link }: { children: ReactNode, title: string, description: string, image: string, link: string }) => {
+const ProjectCard = ({ children, title, description, image, link, mobile, dekstop }: { children: ReactNode, title: string, description: string, image: string, link: string, mobile: string, dekstop: string }) => {
   return (
-    <div className='mx-auto flex relative w-[96%] bg-zinc-900 h-[48rem] md:h-96 mt-10 rounded-lg'>
+    <div className='mx-auto flex relative flex-col md:flex-row w-[96%] bg-zinc-900 h-full md:h-96 mt-10 rounded-lg'>
       <div className='w-full md:w-1/2 p-5 h-full'>
-        <p className='font-extrabold text-3xl'>{ title }</p>
-        <p className='mt-2 font-semibold mb-4'>{ description }</p>
+        <p className='font-extrabold text-3xl'>{title}</p>
+        <p className='mt-2 font-semibold mb-4'>{description}</p>
         <div className='flex gap-5 flex-wrap'>
-          { children }
+          {children}
         </div>
         <div onClick={() => openInNewTab(link)} className='mt-5 px-4 rounded-full w-fit flex items-center justify-center gap-1 py-1 bg-zinc-800 cursor-pointer'>
           <img className='size-10' src={image} alt="" />
           <p>View More</p>
         </div>
       </div>
-      <div className='w-1/2 flex justify-end items-end'>
-        <div className='w-5/6 h-5/6 bg-red-300'>
-
+      <div className='pb-5 md:pb-0 md:w-1/2 flex justify-end items-end'>
+        <div className='w-5/6 h-5/6 mx-auto md:mx-0 relative rounded-ss-lg'>
+          <img className='h-full w-full object-cover rounded-md md:rounded-none md:rounded-ss-lg' src={dekstop} alt="" />
+          <div className='w-1/3 h-5/6 absolute left-3 bottom-3 rounded-md md:rounded-none rounded-se-lg'>
+            <img className='h-full' src={mobile} alt="" />
+          </div>
         </div>
       </div>
       <BorderBeam borderWidth={3} size={100} duration={10} delay={9} />
--- src/components/molecules/project-group/ProjectGroup.tsx
+++ src/components/molecules/project-group/ProjectGroup.tsx
@@ -3,9 +3,13 @@ import Title from '../../atoms/title/Title'
 import { PulsatingButton } from '../../atoms/pulsing-button/PulsingButton'
 import { openInNewTab } from '../../../lib/utils'
 import Tag from '../../atoms/tag/Tag'
+import ProjectCard from '../../atoms/project-card/ProjectCard'
 
 import github from '../../../assets/icon/github.svg'
-import ProjectCard from '../../atoms/project-card/ProjectCard'
+import ceritaNikahMobile from '../../../assets/images/cerita-nikah-mobile.png'
+import ceritaNikahDekstop from '../../../assets/images/cerita-nikah-dekstop.png'
+import jucetipMobile from '../../../assets/images/juicetip-mobile.png'
+import juicetipDekstop from '../../../assets/images/juicetip-dekstop.png'
 
 const ProjectGroup = () => {
   return (
@@ -16,6 +20,8 @@ const ProjectGroup = () => {
         description='Cerita Nikah is a platform that provides digital wedding invitation creation services. Users can choose from various packages and invitation templates that can be freely customized, giving them greater access and flexibility in the creation process. 💌'
         image={github}
         link='https://github.com/stevenaruu/Cerita-Nikah'
+        mobile={ceritaNikahMobile}
+        dekstop={ceritaNikahDekstop}
       >
         <Tag>React</Tag>
         <Tag>Typescript</Tag>
@@ -27,6 +33,8 @@ const ProjectGroup = () => {
         description='JuiceTip is a user-friendly web-based platform to facilitate international entrustment services. The platform aims to simplify the process of entrusting goods by providing a secure and efficient environment for users to search, order, and send items from various locations around the world. 🌏'
         image={github}
         link='https://github.com/stevenaruu/JuiceTip-FE'
+        mobile={jucetipMobile}
+        dekstop={juicetipDekstop}
       >
         <Tag>React</Tag>
         <Tag>Typescript</Tag>
